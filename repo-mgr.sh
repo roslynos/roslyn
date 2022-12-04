f11387a6b593b9d4f43ad91ce336145f8c07eddc
Bytewizer <bytewizer@gmail.com>
2022-12-04 13:14:06 -0800

    test

--- repo-mgr.sh
+++ repo-mgr.sh
@@ -44,14 +44,10 @@ _parse_conf() {
     repo_branch=( $(grep ^repository $conf_file | cut -d ',' -f4) )
 }
 
-locale() {
-    sudo apt-get install language-pack-en
-    locale-gen en_US.UTF-8
-}
-
 install() {
     echo -e "Installing dependencies\n"
     sudo apt-get update
+    sudo apt-get install -y language-pack-en
     sudo apt-get install -y gawk wget git diffstat unzip texinfo gcc build-essential \
         chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils \
         iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev \
