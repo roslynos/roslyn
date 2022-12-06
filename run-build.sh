60933314ed8b6da451d74d73ec93033a645a99a8
Bytewizer <bytewizer@gmail.com>
2022-12-06 07:13:20 -0800

    update build

--- run-build.sh
+++ run-build.sh
@@ -8,12 +8,12 @@ BUILD_DIR="build"
 source ./poky/oe-init-build-env $BUILD_DIR
 
 # Build
-MACHINE=raspberrypi3-64 bitbake dotnet-image-minimal --dry-run
-MACHINE=raspberrypi3-64 bitbake dotnet-image-full --dry-run
-MACHINE=raspberrypi4-64 bitbake dotnet-image-minimal --dry-run
-MACHINE=raspberrypi4-64 bitbake dotnet-image-full --dry-run
-MACHINE=reterminal-64 bitbake dotnet-image-minimal --dry-run
-MACHINE=reterminal-64 bitbake dotnet-image-full --dry-run
+MACHINE=raspberrypi3-64 bitbake dotnet-image-minimal
+MACHINE=raspberrypi3-64 bitbake dotnet-image-full
+MACHINE=raspberrypi4-64 bitbake dotnet-image-minimal
+MACHINE=raspberrypi4-64 bitbake dotnet-image-full
+MACHINE=reterminal-64 bitbake dotnet-image-minimal
+MACHINE=reterminal-64 bitbake dotnet-image-full
 
 # Create the package index
 bitbake package-index
