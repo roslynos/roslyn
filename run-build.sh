535f850772a70d40c72ebcc05b07b8df21906e58
bytewizer <bytewizer@gmail.com>
2023-01-06 09:25:43 -0800

    update build

--- run-build.sh
+++ run-build.sh
@@ -10,10 +10,20 @@ source ./poky/oe-init-build-env $BUILD_DIR
 # Build
 # MACHINE=raspberrypi3-64 bitbake dotnet-image-minimal
 # MACHINE=raspberrypi3-64 bitbake dotnet-image-full
-# MACHINE=raspberrypi4-64 bitbake dotnet-image-minimal
-# MACHINE=raspberrypi4-64 bitbake dotnet-image-full
-MACHINE=reterminal-64 bitbake dotnet-image-minimal
-# MACHINE=reterminal-64 bitbake dotnet-image-full
 
 # Create the package index
-bitbake package-index
+# MACHINE=raspberrypi3-64 bitbake package-index
+
+# MACHINE=raspberrypi4-64 bitbake dotnet-image-minimal
+# MACHINE=raspberrypi4-64 bitbake dotnet-image-full
+
+# Create the package index
+# MACHINE=raspberrypi4-64 bitbake package-index
+
+MACHINE=reterminal-64 bitbake dotnet-image-minimal
+MACHINE=reterminal-64 bitbake dotnet-image-full
+
+# Create the package index
+MACHINE=reterminal-64 bitbake package-index
+
+# rsync -av ~/roslyn/build/tmp/deploy/deb root@whisk.roslynos.com:/var/www/html
